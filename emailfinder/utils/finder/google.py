d0df781c2e726977274ac69b6e0340ed29ce029d
ymx <ymx@autobot.com>
2022-12-07 17:44:53 +0800

    .

--- emailfinder/utils/finder/google.py
+++ emailfinder/utils/finder/google.py
@@ -34,7 +34,7 @@ def search(target, proxies=None, total=50):
         try:
             response = requests.get(ip_check_url, proxies={'http': "http://" + ip}, timeout=5)
             print_info(f"IP: {ip} - {response.text.strip()}")
-            if response.text.strip() == ip.split(':')[0]:
+            if response.status_code == 200:
                 print_ok(f"当前代理IP：{ip}")
                 ip_can_use = ip
                 break
