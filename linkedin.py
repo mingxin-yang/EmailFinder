69dfe341d9e3a100c17761243ee4f7a8c37967d2
ymx <ymx@home.com>
2023-02-15 01:18:20 +0800

    .

--- linkedin.py
+++ linkedin.py
@@ -163,6 +163,7 @@ def search_with_google(target, proxies=None, total=10):
     link_dict = {target: links_2[0] if len(links_2) > 0 else None}
     link_list_dict = [].append(link_dict)
     print("link_list_dict")
+    print(link_dict)
     print(link_list_dict)
     return link_list_dict
 
